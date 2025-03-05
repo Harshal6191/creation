886fa2037f4fed250107899b7d47ba2f4195079a
Harshal6191 <156218037+Harshal6191@users.noreply.github.com>
2025-03-06 00:11:15 +0530

    script.js

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 var date = new Date();
 
 
-var hours = date.getHours();
+setTimeInterval(()={
+  var hours = date.getHours();
 var minutes = date.getMinutes();
 var seconds = date.getSeconds();
 
 
 var time = hours + ":" + minutes + ":" + seconds;
 
+
 document.getElementById("rectangle").innerHTML = time;
+},1000)
+
